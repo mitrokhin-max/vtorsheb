a29b842906487bde45c4b8b3106edad83f98e7d9
Max <mitrohmax@yandex.ru>
2020-10-26 15:08:01 +0300

    11

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
         event.preventDefault();
         var id  = $(this).attr('href'),
             top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 1000);
+        $('body,html').animate({scrollTop: top - 70}, 1000);
     });
     
     /* Кнопка вниз */
